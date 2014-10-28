9347e5e46012fd778ae395c37bba1a44827539fa
Tom Pesman <tom@tnux.net>
2014-10-28 14:30:35 +0100

    improve access_token spec

--- spec/lib/pushr/daemon/wns_support/access_token_spec.rb
+++ spec/lib/pushr/daemon/wns_support/access_token_spec.rb
@@ -5,14 +5,23 @@ require 'pushr/daemon/wns_support/access_token'
 describe Pushr::Daemon::WnsSupport::AccessToken do
   subject { Pushr::Daemon::WnsSupport::AccessToken.new(config) }
 
+  let(:config) do
+    Pushr::ConfigurationWns.new(app: 'app_name', connections: 2, enabled: true, client_id: client_id,
+                                client_secret: client_secret)
+  end
   describe 'receives token' do
-    let(:config) do
-      Pushr::ConfigurationWns.new(app: 'app_name', connections: 2, enabled: true, client_id: 'test',
-                                  client_secret: 'secret')
-    end
-
+    let(:client_id) { 'test' }
+    let(:client_secret) { 'secret' }
     it 'succesful', :vcr do
       expect(subject.get).to eql('acces_token')
+    end
+  end
+
+  describe 'does not receive token' do
+    let(:client_id) { 'invalid_test' }
+    let(:client_secret) { 'invalid_secret' }
+    it 'unsuccesful', :vcr do
+      expect { subject.get }.to raise_error(Pushr::Daemon::WnsSupport::AuthenticationError)
     end
   end
 end
